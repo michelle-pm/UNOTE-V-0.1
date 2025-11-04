0a872955f4bf2523540824ae8dfccb3d7c98f84a
michelle-pm <138757611+michelle-pm@users.noreply.github.com>
2025-11-04 20:42:42 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ["react", "react-dom"],
+  },
   optimizeDeps: {
     include: [
       "firebase/app",
       "firebase/auth",
       "firebase/firestore",
-      "firebase/storage"
+      "firebase/storage",
     ],
   },
   build: {
@@ -20,7 +22,4 @@ export default defineConfig({
       transformMixedEsModules: true,
     },
   },
-  resolve: {
-    dedupe: ["firebase"],
-  },
-})
+});
