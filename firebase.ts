11f3255e9dd615f6d5d8492bbd36b34c229cc5eb
michelle-pm <138757611+michelle-pm@users.noreply.github.com>
2025-11-04 20:32:12 +0300

    feat: Add Firebase Storage to client Includes the necessary import and initialization for Firebase Storage, enabling future features that leverage cloud storage.

--- firebase.ts
+++ firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB3lM1fE7tbMAtDwWS1ZJT2Q2KAVo9Tirc",
@@ -18,3 +19,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
